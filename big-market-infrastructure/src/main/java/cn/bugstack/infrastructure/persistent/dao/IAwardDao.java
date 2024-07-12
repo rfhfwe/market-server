a33ba61ba609b2d81041ce520599f6146549c607
swy-yue <2993266531@qq.com>
2024-07-12 21:40:07 +0800

    feat: 实现抽奖的概率处理，连接redis

--- big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IAwardDao.java
+++ big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IAwardDao.java
@@ -5,8 +5,13 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 奖品表DAO
+ * @create 2023-12-16 13:23
+ */
 @Mapper
-public interface IAWardDao {
+public interface IAwardDao {
 
     List<Award> queryAwardList();
 
