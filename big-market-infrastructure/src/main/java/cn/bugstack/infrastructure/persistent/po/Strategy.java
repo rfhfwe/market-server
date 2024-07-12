2b0ac206c63a7aabb6ede3b5f30654f860dd578b
swy-yue <2993266531@qq.com>
2024-07-12 16:52:23 +0800

    feat: 持久层实体和dao编写

---
+++ big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/po/Strategy.java
@@ -0,0 +1,22 @@
+package cn.bugstack.infrastructure.persistent.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Strategy {
+
+    private Long id;
+    private Long strategyId;
+    private String strategyDesc;
+    private Date createTime;
+    private Date updateTime;
+
+}
